5e0666927c8aad5fb9f7af480b59f51bf825e911
MikkelNf <mikkel.nf@gmail.com>
2022-12-25 20:19:50 +0700

    done crud family-member

--- api.js
+++ api.js
@@ -27,8 +27,26 @@ app.post('/family-members', (req,res) => {
     // res.json(familyMemberLevelId)
     client.query(`insert into family_members (name, gender, family_member_level_id) values ('${name}', '${gender}', '${familyMemberLevelId}')`, (err, result) => {
         if(!err){
-            // res.status(200).json(result.rows)
             res.send('insert success')
+        }
+    })
+})
+
+app.put('/family-members/:id', (req,res) => {
+    const {name, gender, familyMemberLevelId} = req.body
+    let id = req.params.id
+    client.query(`update family_members set name = '${name}', gender = '${gender}', family_member_level_id = '${familyMemberLevelId}' where family_member_id = ${id}`, (err, result) => {
+        if(!err){
+            res.send('update success')
+        }
+    })
+})
+
+app.delete('/family-members/:id', (req,res) => {
+    let id = req.params.id
+    client.query(`DELETE FROM family_members where family_member_id = ${id}`, (err, result) => {
+        if(!err){
+            res.send('delete success')
         }
     })
 })
